caab85d2ee22ee8e1e3b46a175e7c1dfa05a8b95
Chakshu Gupta <chakshugupta@gmail.com>
2024-06-04 15:52:10 +0200

    Move verify func to main

--- main.py
+++ main.py
@@ -5,8 +5,33 @@ import yaml
 
 from src.object.base_model import BaseModel
 from src.traffic_process import preprocess_traffic
-from src.util import verify_config
 
+
+def verify_config(config):
+    """
+    Verify the entries in the config file pass the basic sanity checks
+    """
+    error = False
+
+    if "dataset-path" not in config:
+        error = True
+    else:
+        if not os.path.exists(config["dataset-path"]):
+            error = True
+        elif not os.path.isdir(config["dataset-path"]):
+            error = True
+    
+    if "device-file" not in config:
+        error = True
+    else:
+        if not os.path.exists(config["device-file"]):
+            error = True
+        elif not os.path.isfile(config["device-file"]):
+            error = True
+        elif not config["device-file"].endswith(".txt"):
+            error = True
+    
+    return error
 
 
 def load_device_file(device_file):
